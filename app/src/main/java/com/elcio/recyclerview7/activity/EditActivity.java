1c22dd76de60aac87b84c976203a2bfc09bea609
Elcio Cestari Taira <elciotaira@gmail.com>
2020-10-09 12:45:11 -0400

    add a second activity called EditActivity, and add too a FAB, and they functionality, which is start this new activity.

---
+++ app/src/main/java/com/elcio/recyclerview7/activity/EditActivity.java
@@ -0,0 +1,16 @@
+package com.elcio.recyclerview7.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.elcio.recyclerview7.R;
+
+public class EditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+}
--- app/src/main/java/com/elcio/recyclerview7/activity/MainActivity.java
+++ app/src/main/java/com/elcio/recyclerview7/activity/MainActivity.java
@@ -1,16 +1,39 @@
 package com.elcio.recyclerview7.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.elcio.recyclerview7.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    FloatingActionButton fAB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        fAB = findViewById(R.id.floatingActionButton);
+
+        fAB.setOnClickListener(fABListner());
+    }
+
+    private View.OnClickListener fABListner() {
+
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EditActivity.class);
+                startActivity(intent);
+            }
+        };
     }
 }
